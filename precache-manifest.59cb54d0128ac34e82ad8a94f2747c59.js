3b2e265c365f309dfe4f4066d29e593acbe2f14a
vytamini <vytamini1@gmail.com>
2019-01-09 17:49:54 +0100

    Updates

---
+++ precache-manifest.59cb54d0128ac34e82ad8a94f2747c59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0434f82eedbbe0c40b23",
+    "url": "/reactV2/static/css/main.b0e34a28.chunk.css"
+  },
+  {
+    "revision": "0434f82eedbbe0c40b23",
+    "url": "/reactV2/static/js/main.0434f82e.chunk.js"
+  },
+  {
+    "revision": "24238c8751c239a5a46f",
+    "url": "/reactV2/static/js/1.24238c87.chunk.js"
+  },
+  {
+    "revision": "69590f7a512ae7c35964",
+    "url": "/reactV2/static/js/runtime~main.69590f7a.js"
+  },
+  {
+    "revision": "d9dcb9a5d6ed4a126b54e379f2d2abcd",
+    "url": "/reactV2/static/media/apel.d9dcb9a5.png"
+  },
+  {
+    "revision": "c0d200ea36f8e401ff7b7394884acea0",
+    "url": "/reactV2/static/media/chokolade.c0d200ea.png"
+  },
+  {
+    "revision": "2dcb676435e6137fd6b9c58c3ff4d10e",
+    "url": "/reactV2/static/media/karneval.2dcb6764.png"
+  },
+  {
+    "revision": "c89c7ac0c3799f6cdb2925c6bfd8a940",
+    "url": "/reactV2/index.html"
+  }
+];
